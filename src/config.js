36a7062458a678d158fc0d0c2354a5b10171dac0
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-09-07 15:55:23 -0400

    updated config api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-export default {
-  //API_ENDPOINT: 'https://petful-a-m.herokuapp.com/api'
-  API_ENDPOINT: 'http://localhost:8080/api'
- 
+export default {  
+  API_ENDPOINT: 'https://petful-a-m.herokuapp.com/api'
 }
